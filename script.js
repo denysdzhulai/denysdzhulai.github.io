160cd0d5e96dd3e0ae423efb3103fb4ff033493d
Denys Dzhulai <denisjuly@gmail.com>
2022-10-23 13:16:01 -0600

    Modified "date" indication in footer

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 window.onload = function () {
-    document.body.classList.remove("is-preload");
-  };
-  window.ontouchmove = function () {
-    return false;
-  };
-  window.onorientationchange = function () {
-    document.body.scrollTop = 0;
-  };
+  document.body.classList.remove("is-preload");
+};
+window.ontouchmove = function () {
+  return false;
+};
+window.onorientationchange = function () {
+  document.body.scrollTop = 0;
+};
+
+function date() {
+  document.getElementById("date").innerHTML = new Date().getFullYear();
+}
+date();
